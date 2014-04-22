ae3579093b6060201e5256e4dc9a302e7b8c72c7
evan graham <adidaska@gmail.com>
2014-04-22 15:47:38 -0400

    added new class

---
+++ Git Test/MEGTestClass.h
@@ -0,0 +1,13 @@
+//
+//  MEGTestClass.h
+//  Git Test
+//
+//  Created by Evan Graham on 4/22/14.
+//  Copyright (c) 2014 MeG Studios. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MEGTestClass : NSObject
+
+@end
---
+++ Git Test/MEGTestClass.m
@@ -0,0 +1,13 @@
+//
+//  MEGTestClass.m
+//  Git Test
+//
+//  Created by Evan Graham on 4/22/14.
+//  Copyright (c) 2014 MeG Studios. All rights reserved.
+//
+
+#import "MEGTestClass.h"
+
+@implementation MEGTestClass
+
+@end
